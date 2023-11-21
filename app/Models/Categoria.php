6b79d5b0bab5b4ea821af5e0fe1e552df6c96b2a
CarlosEduardoAO <18032477@itcelaya.edu.mx>
2023-11-21 14:13:55 -0600

    EstadoSolicitud Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -11,6 +11,11 @@ class Categoria extends Model
 
     protected $fillable = ['nombre', 'descripcion'];
 
+    public function estadoSolicitud()
+    {
+        return $this->hasOne(EstadoSolicitud::class, 'id', 'estado_solicitud_id');
+    }
+
     public function productos()
     {
         return $this->hasMany(Producto::class); // Una categoria puede tener muchos productos asociados.
